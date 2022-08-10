d2a0416f77dd82a8b5a382ae23862180ade8b185
Juan Castillo <Juan.Castillo@biophys.mpg.de>
2022-08-10 15:49:19 +0200

    Delete run_AF_NOX2_p22.sh

--- scripts/old/run_AF_NOX2_p22.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
- 
- module load python-3.7.3 
-sudo python3 /home/alphafold/alphafold/docker/run_docker.py --fasta_paths=/home/alphafold/fastas/NOX2_p22.fasta --max_template_date=2022-08-01 --model_preset=multimer --data_dir=/home/alphafold/genetic_database_new/ --output_dir=/home/alphafold/results/
