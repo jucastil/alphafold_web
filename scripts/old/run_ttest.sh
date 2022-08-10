0c2e127602ed30e20cb6fc11813294ac949ad430
Juan Castillo <Juan.Castillo@biophys.mpg.de>
2022-08-10 15:52:57 +0200

    Delete run_ttest.sh

--- scripts/old/run_ttest.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
- 
- module load python-3.7.3 
-sudo python3 /home/alphafold/alphafold/docker/run_docker.py --fasta_paths=/home/alphafold/fastas/sapiens5.FASTA --max_template_date=2022-05-07 --model_preset=monomer --data_dir=/home/alphafold/genetic_database_new/ --output_dir=/home/alphafold/results/
