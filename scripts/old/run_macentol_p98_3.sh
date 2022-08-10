7d1db914cde3af229e33a6bcda75f113177b09e8
Juan Castillo <Juan.Castillo@biophys.mpg.de>
2022-08-10 15:51:48 +0200

    Delete run_macentol_p98_3.sh

--- scripts/old/run_macentol_p98_3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
- 
- module load python-3.7.3 
-sudo python3 /home/alphafold/alphafold/docker/run_docker.py --fasta_paths=/home/alphafold/fastas/Y98_SIRV1_3.fasta --max_template_date=2021-11-01 --model_preset=multimer --data_dir=/home/alphafold/genetic_database_new/ --output_dir=/home/alphafold/results/
